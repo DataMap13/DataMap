07ae0e512daac72b42d1ab343e61c46ca90c9318
Justin Hummel <justin@CentralServer.(none)>
2013-04-23 16:33:44 -0400

    Added column for taking action with the collection nodes through the web interface.

--- web/node_management/javascript.js
+++ web/node_management/javascript.js
@@ -26,19 +26,27 @@ function getStatus() {
 				header = table.createTHead();
 				header_row = header.insertRow(0);
 				for (var i = 0; i < status_table_headers.length; i++) {
-					cell = header_row.insertCell(i);
+					var cell = header_row.insertCell(i);
 					cell.innerHTML = status_table_headers[i][0];
 				}
+				var cell = header_row.insertCell(status_table_headers.length);
+				cell.innerHTML = "Action";
 				
 				body = table.createTBody();
 				for (var i = 0; i < data.length; i++) {
+					var state;
 					var row = body.insertRow(i);
 					for (var j in status_table_headers) {
 						var cell = row.insertCell(j);
 						cell.innerHTML = data[i][status_table_headers[j][1]];
-						if (status_table_headers[j][1] == "state")
-							cell.className = data[i][status_table_headers[j][1]];
+						if (status_table_headers[j][1] == "state") {
+							state = data[i][status_table_headers[j][1]];
+							cell.className = state;
+						}
 					}
+					var cell = row.insertCell(status_table_headers.length);
+					var actionSelect = createActionSelect(state);
+					cell.appendChild(actionSelect);
 				}
 			
 				tableArea.innerHTML = "";
@@ -47,5 +55,30 @@ function getStatus() {
 			}
 		});
 	});
+
+}
+
+function createActionSelect(state) {
+
+	var select = document.createElement("select");
+	select.add(new Option(" - Action - "));
+	
+	switch(state) {
+		case "DISCONNECTED":
+			select.add(new Option("Remove", "Remove"));
+			break;
+		case "CONNECTED":
+			select.add(new Option("Start", "Start"));
+			break;
+		case "COLLECTING":
+			select.add(new Option("Stop", "Stop"));
+			break;
+		case "ERROR":
+			select.add(new Option("Start", "Start"));
+			select.add(new Option("Stop", "Stop	"));
+			break;
+	}
+	
+	return select;
 
 }

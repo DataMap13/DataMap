99ee222fc351715e5007f16bdb2dbefb0c87c9b8
Justin Hummel <justin@CentralServer.(none)>
2013-04-24 19:36:18 -0400

    Added buttons for starting and stopping all nodes.

--- web/node_management/index.php
+++ web/node_management/index.php
@@ -6,6 +6,11 @@ $on_load = "getStatus(); setInterval('getStatus();', 5000);";
 require "../common/top.php";
 ?>
 
+<p>
+	<button onclick="requestAction('start all');">Start All Nodes</button>
+	<button onclick="requestAction('stop all');">Stop All Nodes</button>
+</p>
+
 <div id="status_table_area">Loading...</span>
 
 <?php require "../common/bottom.php"; ?>
